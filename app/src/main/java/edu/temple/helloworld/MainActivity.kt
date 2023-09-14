52e57d1c97e291846caf1da47cf66eff7242edcd
nickc <tug61145@temple.edu>
2023-09-14 12:19:30 -0400

    Added event listener for the button.

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
+    lateinit var displayEditTextView: EditText
+    lateinit var clickMeButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +21,26 @@ class MainActivity : AppCompatActivity() {
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
 
+        // Edit text field view reference
+        displayEditTextView = findViewById(R.id.nameEditText)
+
+        // Button text view reference
+        clickMeButton = findViewById(R.id.clickMeButton)
+
+        // on click function
+        clickMeButton.setOnClickListener() {
+            var textFromEdit = displayEditTextView.text
+            var editMessage = "Hello, $textFromEdit"
+            displayTextView.text = editMessage
+        }
+
+
+
+
         
         // Respond to button click event per specifications
 
 
     }
 }
+
